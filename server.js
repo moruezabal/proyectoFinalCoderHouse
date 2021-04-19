eb8656b2e3a9acf070c110270b1692f8769ae033
Mauricio Oruezabal <moruezabal@gmail.com>
2021-04-19 20:16:13 -0300

    cambios minimos server

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'));
 
 const routerProducto = express.Router();
 const routerCarrito = express.Router();
@@ -12,9 +13,11 @@ const routerCarrito = express.Router();
 app.use('/productos', routerProducto);
 app.use('/carrito', routerCarrito);
 
+let repositorio = new Inventario();
 
 
-routerProducto.get('/', (req, res) => {
+
+routerProducto.get('/listar:id?', (req, res) => {
     res.send('Hello Producto!')
 })
 
